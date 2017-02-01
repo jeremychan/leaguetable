71b28ed59a5484a5de0511681dee1be5f93f34df
Jeremy Chan <chunwing2@hotmail.com>
2017-02-02 01:33:43 +0800

    added tests for regex that will be used to construct result object from string

---
+++ server/src/result.js
@@ -0,0 +1,16 @@
+export default class Result {
+
+    constructor(input) {
+
+        this.home = {
+            p1: 'AB',
+            p2: 'CD',
+            score: 2
+        };
+        this.away = {
+            p1: 'EF',
+            p2: 'GH',
+            score: 1
+        }
+    }
+}
---
+++ server/test/result_spec.js
@@ -0,0 +1,77 @@
+import {expect} from 'chai';
+
+import Result from '../src/result'
+
+describe('result construction logic', () => {
+
+    describe('extract input with regex', () => {
+
+        const re = /^([^\s]+?)\s+([^\s]+?)\s+(\d+?)\s*-\s*(\d+?)\s+([^\s]+?)\s+([^\s]+)$/;
+
+        it('basic format', () => {
+            const input = 'AB CD 2 - 1 EF GH';
+            const result = re.exec(input);
+            expect(result[1]).to.equal('AB');
+            expect(result[2]).to.equal('CD');
+            expect(result[3]).to.equal('2');
+            expect(result[4]).to.equal('1');
+            expect(result[5]).to.equal('EF');
+            expect(result[6]).to.equal('GH');
+        });
+
+        it('with spaces', () => {
+            const input = ' AB  CD    2  -  1   EF  GH  ';
+            const result = re.exec(input.trim());
+            expect(result[1]).to.equal('AB');
+            expect(result[2]).to.equal('CD');
+            expect(result[3]).to.equal('2');
+            expect(result[4]).to.equal('1');
+            expect(result[5]).to.equal('EF');
+            expect(result[6]).to.equal('GH');
+        });
+
+        it('extra player', () => {
+            const input = ' AB  CD  E  2  -  1   EF  GH  ';
+            const result = re.exec(input.trim());
+            expect(result).to.be.null;
+        });
+
+        it('missing score', () => {
+            const input = 'AB CD 2 -  EF GH';
+            const result = re.exec(input.trim());
+            expect(result).to.be.null;
+        });
+
+        it('no space in score', () => {
+            const input = 'AB CD 2-1 EF GH';
+            const result = re.exec(input.trim());
+            expect(result[1]).to.equal('AB');
+            expect(result[2]).to.equal('CD');
+            expect(result[3]).to.equal('2');
+            expect(result[4]).to.equal('1');
+            expect(result[5]).to.equal('EF');
+            expect(result[6]).to.equal('GH');
+        });
+    });
+
+    describe('construction', () => {
+
+        it('construct result object', () => {
+            const input = 'AB CD 2 - 1 EF GH';
+            const result = new Result(input);
+            expect(result).to.deep.equal({
+                home: {
+                    p1: 'AB',
+                    p2: 'CD',
+                    score: 2
+                },
+                away: {
+                    p1: 'EF',
+                    p2: 'GH',
+                    score: 1
+                }
+            });
+        });
+    });
+
+});
